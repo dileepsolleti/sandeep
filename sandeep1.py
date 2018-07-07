6a567878e2b43a77672c8ce0c353974fe6b6b6cd
Yunns123 <40906513+Yunns123@users.noreply.github.com>
2018-07-07 10:01:50 +0530

    Update sandeep1.py
    sandeep

--- sandeep1.py
+++ sandeep1.py
@@ -1,5 +1,8 @@
-num = int(input(" "))
-if (num % 2) == 0:
-   print("Even".format(num))
+num = int(input())
+if num>=0 and num<=10000 :
+  if (num % 2) == 0:
+    print("Even")
+  else:
+   print("Odd") 
 else:
-   print("Odd".format(num)) 
+   print("invalid")
